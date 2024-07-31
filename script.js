a4bf59a56f753f392a53233046e7919dcb6789b7
Varick Tecson <varicktecson@gmail.com>
2024-07-30 21:19:48 -0400

    block07workshop

---
+++ script.js
@@ -0,0 +1,16 @@
+// Created a string for the user to receive once user opens the page
+
+const msg ="You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+
+/*
+Variables assigned for the three combo numbers.  Each variable constains the corresponding result of calculation using a unique 
+arithmetic operator +-*.  Each equals one of the three combination codes
+*/
+
+const num1 = 5 * 2
+const num2 = 2 * 20
+const num3 = 40 - 1
+
+// Dialogue popup alert that displays the vault codes 
+
+alert(`${msg} is ${num1} - ${num2} - ${num3}`)
